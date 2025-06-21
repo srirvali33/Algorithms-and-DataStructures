a038be7384a076c168c03cb5f120fed3965d29cd
srirvali33 <48911704+srirvali33@users.noreply.github.com>
2025-06-21 14:34:51 -0700

    Time: 1 ms (10.34%) | Memory: 56.1 MB (97.67%) - LeetSync

---
+++ 2-sum-problem/792-binary-search/binary-search.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+
+
+
+   
+    var left = 0;
+    var right= nums.length-1;
+    var middle=0;
+    //var middle= Math.ceil(nums.length/2);
+    while(left <= right){
+
+        middle=Math.floor((right-left/2));
+
+        let midVal=nums[middle]
+
+        if(midVal===target){
+            return middle;
+        }
+
+        else if(target < midVal){
+            right=middle-1;
+        }
+
+        else if (target > midVal){
+            left=middle+1;
+        }
+
+    }
+
+    return -1;
+    }
