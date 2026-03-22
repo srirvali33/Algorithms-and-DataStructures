f9d1b5a1dd4f1db49d9cb32601780ba3a42a8fa4
srirvali33 <48911704+srirvali33@users.noreply.github.com>
2026-03-22 15:58:49 -0700

    Time: 0 ms (100.00%) | Memory: 53.9 MB (51.03%) - LeetSync

---
+++ 2-sum-problem/35-search-insert-position/search-insert-position.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+
+    let start=0;
+    let end= nums.length-1;
+    let mid=0;
+
+    while(start <= end){
+        mid= Math.floor(start+(end-start)/2);
+        if(nums[mid]==target){
+            return mid;
+        }else if(nums[mid] > target){
+            end=mid-1;
+        }else{
+            start=mid+1
+        }
+      
+    }
+
+    return (target > nums[mid]) ? mid + 1 : mid;
+};
