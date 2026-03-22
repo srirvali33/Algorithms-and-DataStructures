eeb9841fe1e1117a33fd7611f6c39b17b30c51f2
srirvali33 <48911704+srirvali33@users.noreply.github.com>
2026-03-21 18:24:20 -0700

    Time: 0 ms (100.00%) | Memory: 58.3 MB (46.79%) - LeetSync

--- 2-sum-problem/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ 2-sum-problem/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -3,27 +3,22 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    let currIndex=0;
-    let totalUnique=0;
+     let currIndex=0;
+     let nextIndex=1;
 
-    let nextIndex=1;
+     while(nextIndex < nums.length){
 
-    while(nextIndex <= nums.length){
-
-        if(nums[currIndex]== nums[nextIndex] ){
+        if(nums[currIndex]== nums[nextIndex]){
             nextIndex=nextIndex+1;
         }else{
             currIndex=currIndex+1;
             nums[currIndex]=nums[nextIndex];
-            totalUnique=totalUnique+1;
             nextIndex=nextIndex+1;
-            
         }
 
-    }
+     }
 
-    return totalUnique;
-
+     return currIndex+1;
 
     
 };
