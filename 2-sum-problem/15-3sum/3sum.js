7970f8e9f89cea0361dc347b2abfa14b81bd7965
srirvali33 <48911704+srirvali33@users.noreply.github.com>
2026-03-22 15:16:48 -0700

    Time: 360 ms (8.79%) | Memory: 73.2 MB (24.61%) - LeetSync

---
+++ 2-sum-problem/15-3sum/3sum.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+
+    
+var threeSum = function(nums) {
+
+    let resList=[];
+    nums.sort((a, b) => a - b);
+
+
+    function twoSum(i){
+    let seen = new Set();
+    for (let j = i + 1; j < nums.length; j++) {
+      const target = 0 - nums[i] - nums[j];
+      if (seen.has(target)) {
+        resList.push([nums[i], nums[j], target]);
+        while (nums[j] === nums[j+1]) j++;
+      }
+      seen.add(nums[j]);
+    }
+    }
+
+
+    for (let i=0; i < nums.length; i++){
+        if (nums[i] > 0) break;
+        if (i === 0 || nums[i-1] !== nums[i]) twoSum(i);
+       
+    }
+
+    return resList;
+    
+};
