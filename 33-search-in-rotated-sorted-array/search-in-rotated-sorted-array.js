61bcc070fb76574fd10b3eeb2cc0498e38cd57c5
srirvali33 <48911704+srirvali33@users.noreply.github.com>
2026-03-23 19:59:14 -0700

    Time: 0 ms (100.00%) | Memory: 53.5 MB (83.66%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
@@ -5,21 +5,28 @@
  */
 var search = function(nums, target) {
 
-    let  start= 0;
-    let end =nums.length-1;
+        let low = 0, high = nums.length - 1;
 
-    while(start <= end){
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
 
-       if(nums[start]==target){
-        return start;
-       }else if(nums[end]==target){
-         return end;
-       }else{
-        start=start+1;
-        end=end-1;
-       }
+        if (nums[mid] === target) {
+            return mid;
+        }
 
-
+        if (nums[low] <= nums[mid]) {
+            if (nums[low] <= target && target < nums[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        } else {
+            if (nums[mid] < target && target <= nums[high]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
     }
 
     return -1;
