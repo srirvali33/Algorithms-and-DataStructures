6a0a27784908f41d163020e0c85b8f2a667b17b8
srirvali33 <48911704+srirvali33@users.noreply.github.com>
2026-03-25 13:27:34 -0700

    Time: 0 ms (100.00%) | Memory: 57.6 MB (67.32%) - LeetSync

---
+++ 206-reverse-linked-list/reverse-linked-list.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+
+    var prev = null;
+    var curr = head;
+        while(curr != null){
+            var currnext = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = currnext;
+        }
+        return prev;
+};
