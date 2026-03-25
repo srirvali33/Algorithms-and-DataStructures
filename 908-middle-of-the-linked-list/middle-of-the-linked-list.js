c6c7d07553376e7125c464f0009f347878da7fbf
srirvali33 <48911704+srirvali33@users.noreply.github.com>
2026-03-25 15:48:56 -0700

    Time: 0 ms (100.00%) | Memory: 53 MB (87.38%) - LeetSync

---
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+
+    if(!head) return head;
+    let count=0;
+    let node=head;
+
+    while(node){
+        count++;
+        node=node.next;
+    }
+
+    node=head;
+    let mid=Math.ceil(count/2);
+    while(count>=mid && node){
+        if(count==mid){
+            return node;
+        }else{
+            node=node.next
+        }
+        count--;
+    }
+
+
+};
