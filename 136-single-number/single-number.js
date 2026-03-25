95f94e0998f83329b307d81404faffbbe3219b10
srirvali33 <48911704+srirvali33@users.noreply.github.com>
2026-03-25 12:48:19 -0700

    Time: 3 ms (49.26%) | Memory: 57.8 MB (38.38%) - LeetSync

--- 136-single-number/single-number.js
+++ 136-single-number/single-number.js
@@ -3,15 +3,25 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-   let obj={};
-   for(i of nums){
-    if(!obj.hasOwnProperty(i)){
-        obj[i]=1
-    }else{
-        obj[i]=obj[i]+1;
-    }
-   }
+//    let obj={};
+//    for(i of nums){
+//     if(!obj.hasOwnProperty(i)){
+//         obj[i]=1
+//     }else{
+//         obj[i]=obj[i]+1;
+//     }
+//    }
 
-  return parseInt(Object.entries(obj).filter((i)=> i[1]==1)[0][0]);
+//   return parseInt(Object.entries(obj).filter((i)=> i[1]==1)[0][0]);
+
+
+let res=0;
+
+for (let i of nums){
+
+    res=res^i;
+}
+
+return res;
 
 };
