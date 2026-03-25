8f78b8d31e307dc59931bfcfbcf7cccb51840173
srirvali33 <48911704+srirvali33@users.noreply.github.com>
2026-03-25 12:14:00 -0700

    Time: 1 ms (46.11%) | Memory: 57 MB (82.20%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.js
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.js
@@ -11,40 +11,40 @@
  * @return {number}
  */
 var maxDepth = function (root) {
-    if(!root) return 0;
-    let vis = [root];
-    let depth = 0;
+    // if(!root) return 0;
+    // let vis = [root];
+    // let depth = 0;
 
-    while (vis.length>0) {
-        const levelSize = vis.length;  
-        for (let i = 0; i < levelSize; i++) {
-            let currNode = vis.shift();
-             if (currNode.left) {
-                vis.push(currNode.left);
-            }
-             if (currNode.right) {
-                vis.push(currNode.right);
-            }
-        }
-        depth++;
-    }
-
-    return depth;
-
-    // let depth=0;
-
-    // function treeHelper(root){
-    //     if(!root){
-    //         return 0;
+    // while (vis.length>0) {
+    //     const levelSize = vis.length;  
+    //     for (let i = 0; i < levelSize; i++) {
+    //         let currNode = vis.shift();
+    //          if (currNode.left) {
+    //             vis.push(currNode.left);
+    //         }
+    //          if (currNode.right) {
+    //             vis.push(currNode.right);
+    //         }
     //     }
-
-    //     let leftHeight=treeHelper(root.left);
-    //     let rightHeight=treeHelper(root.right);
-    //     return 1+Math.max(leftHeight,rightHeight);
-
+    //     depth++;
     // }
 
-    // depth=treeHelper(root);
     // return depth;
+
+    let depth=0;
+
+    function treeHelper(root){
+        if(!root){
+            return 0;
+        }
+
+        let leftHeight=treeHelper(root.left);
+        let rightHeight=treeHelper(root.right);
+        return 1+Math.max(leftHeight,rightHeight);
+
+    }
+
+    depth=treeHelper(root);
+    return depth;
 
 };
