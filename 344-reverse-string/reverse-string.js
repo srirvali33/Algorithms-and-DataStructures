33cc7bf2194e7ddcbf89959528363d382153759a
srirvali33 <48911704+srirvali33@users.noreply.github.com>
2026-03-25 15:07:36 -0700

    Time: 1 ms (59.86%) | Memory: 62.1 MB (27.87%) - LeetSync

--- 344-reverse-string/reverse-string.js
+++ 344-reverse-string/reverse-string.js
@@ -3,6 +3,8 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function (s) {
+    //TC O(n/2~n)
+    //SC O(1)
     let temp = '';
     for (let i = 0; i <= s.length - 1; i++) {
         temp = s[s.length - i - 1];
