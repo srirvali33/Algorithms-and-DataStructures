81b293830e7a3c335a9b1206a663df26d96541de
srirvali33 <48911704+srirvali33@users.noreply.github.com>
2026-03-24 20:32:28 -0700

    Time: 0 ms (100.00%) | Memory: 54.3 MB (29.53%) - LeetSync

---
+++ 226-invert-binary-tree/invert-binary-tree.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+
+    if(!root){
+        return null;
+    }
+
+
+    const leftTree = invertTree(root.left);
+    const rightTree= invertTree(root.right);
+
+    root.left=rightTree;
+    root.right=leftTree;
+
+    return root
+    
+};
