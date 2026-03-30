a4c968c7daa8c400c64185501463375085d7f104
srirvali33 <48911704+srirvali33@users.noreply.github.com>
2026-03-30 10:40:32 -0700

    Time: 31 ms (62.97%) | Memory: 66.8 MB (82.00%) - LeetSync

--- 49-group-anagrams/group-anagrams.js
+++ 49-group-anagrams/group-anagrams.js
@@ -4,18 +4,30 @@
  */
 var groupAnagrams = function(strs) {
 
-    let angMap= {};
+    // let angMap= {};
 
-    for(let i of strs){
-        let key= i.split("").sort((a,b)=> a.localeCompare(b)).join("");
-        if(!angMap.hasOwnProperty(key)){
-            angMap[key]=i;
-        }else{
-            let res=angMap[key];
-            angMap[key]=angMap[key]+"&"+i;
+    // for(let i of strs){
+    //     let key= i.split("").sort((a,b)=> a.localeCompare(b)).join("");
+    //     if(!angMap.hasOwnProperty(key)){
+    //         angMap[key]=i;
+    //     }else{
+    //         let res=angMap[key];
+    //         angMap[key]=angMap[key]+"&"+i;
+    //     }
+    // }
+
+    // return Object.values(angMap).map((i)=> i.split("&"));
+
+    const map = new Map();
+
+    for (let str of strs) {
+        const key = str.split('').sort().join('');
+        if (!map.has(key)) {
+            map.set(key, []);
         }
+        map.get(key).push(str);
     }
 
-    return Object.values(angMap).map((i)=> i.split("&"));
+    return Array.from(map.values());
     
 };
