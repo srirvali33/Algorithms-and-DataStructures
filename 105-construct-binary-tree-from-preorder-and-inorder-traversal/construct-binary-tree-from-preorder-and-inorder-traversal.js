d4049db0c372f74beceebd82595a97ceda4bc7f6
srirvali33 <48911704+srirvali33@users.noreply.github.com>
2026-03-29 18:58:21 -0700

    Time: 4 ms (57.89%) | Memory: 59.7 MB (60.17%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.js
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.js
@@ -23,8 +23,11 @@ var buildTree = function(preorder, inorder) {
 
     function buildTreeFromList(start, end){
         if(start > end) return null;
+        // take 1st root or every root from pre order for root detection and creation
         let rootNode= new TreeNode(preorder.shift());
+        // left of the root would be inorder left part of current root
         rootNode.left=buildTreeFromList(start,haset[rootNode.val]-1);
+        // right  of the root would be inorder right part of current root
         rootNode.right=buildTreeFromList(haset[rootNode.val]+1,end);
         return rootNode;
     }
