a63982d6810984da71c6d4309306ac367592a2f6
srirvali33 <48911704+srirvali33@users.noreply.github.com>
2026-03-30 13:10:52 -0700

    Time: 7 ms (56.33%) | Memory: 57.8 MB (64.87%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+
+    let maxlen=0, left=0 , charwindow= new Set();
+
+    for(let i=0; i < s.length; i++){
+        // remove duplicates from window dynamically until it has unique vals from begining
+        while(charwindow.has(s[i])){
+            charwindow.delete(s[left]);
+            left++;
+        }
+
+        charwindow.add(s[i]);
+        maxlen= Math.max(maxlen, charwindow.size);
+    };
+
+    return maxlen;
+    
+};
