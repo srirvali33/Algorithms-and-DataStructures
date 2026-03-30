3947332783afd8d8856e10190be454e69c74ca2d
srirvali33 <48911704+srirvali33@users.noreply.github.com>
2026-03-30 13:16:53 -0700

    Time: 28 ms (16.59%) | Memory: 61.2 MB (14.03%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -4,19 +4,34 @@
  */
 var lengthOfLongestSubstring = function(s) {
 
-    let maxlen=0, left=0 , charwindow= new Set();
+    // let maxlen=0, left=0 , charwindow= new Set();
 
-    for(let i=0; i < s.length; i++){
-        // remove duplicates from window dynamically until it has unique vals from begining
-        while(charwindow.has(s[i])){
-            charwindow.delete(s[left]);
-            left++;
+    // for(let i=0; i < s.length; i++){
+    //     // remove duplicates from window dynamically until it has unique vals from begining
+    //     while(charwindow.has(s[i])){
+    //         charwindow.delete(s[left]);
+    //         left++;
+    //     }
+
+    //     charwindow.add(s[i]);
+    //     maxlen= Math.max(maxlen, charwindow.size);
+    // };
+
+    // return maxlen;
+
+    //optimized by DP
+
+    let lastSeen = {}; // char -> last index
+    let left = 0;
+    let maxlen = 0;
+    for (let i = 0; i < s.length; i++) {
+        let char = s[i];
+        if (lastSeen[char] !== undefined && lastSeen[char] >= left) {
+            left = lastSeen[char] + 1;
         }
-
-        charwindow.add(s[i]);
-        maxlen= Math.max(maxlen, charwindow.size);
-    };
-
+        maxlen = Math.max(maxlen, i - left + 1);
+        lastSeen[char] = i;
+    }
     return maxlen;
     
 };
